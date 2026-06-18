3a0be0bcfb71b0965eb44451a443540546f80461
Kourosh <Kourosh.ilchi@gmail.com>
2026-06-18 19:03:17 +0330

    Add FaceRecognitionHead module declaration
    Introduce a FaceRecognitionHead torch::nn::Module declaration in Project/include/model/Head.h. The header adds #include <string>, a constructor signature (num_channel, dropout, activation), a forward method, member layers (conv1, bn1, conv2, bn2, dropout_layer) and a TORCH_MODULE(FaceRecognitionHead) alias. This is a header-only declaration; implementation should be provided in the corresponding source file.

--- Project/include/model/Head.h
+++ Project/include/model/Head.h
@@ -1,6 +1,23 @@
 #pragma once
 #include <torch/torch.h>
+#include <string>
 
 namespace model {
-    
+    struct FaceRecognitionHead : torch::nn::Module {
+        FaceRecognitionHead(
+            int num_channel, 
+            int dropout, 
+            std::string activation = "relu" 
+        );
+
+        torch::Tensor forward(torch::Tensor x);
+
+        torch::nn::Conv2d conv1{nullptr};
+        torch::nn::BatchNorm2d bn1{nullptr};
+        torch::nn::Conv2d conv2{nullptr};
+        torch::nn::BatchNorm2d bn2{nullptr};
+        torch::nn::Dropout dropout_layer{nullptr};
+        
+    };
+    TORCH_MODULE(FaceRecognitionHead);
 }
