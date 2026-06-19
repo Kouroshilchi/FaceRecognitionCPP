34d1f7526cecf84f854f3329d0fdaeb88e9d0144
Kourosh <Kourosh.ilchi@gmail.com>
2026-06-19 10:25:59 +0330

    feat - implementing the FaceRecognitionBody in body.h

---
+++ Project/include/model/Body.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <torch/torch.h>
+#include "ResBlock.h"
+#include <string>
+
+namespace model {
+    struct FaceRecognitionBody : torch::nn::Module {
+        FaceRecognitionBody(
+            int in_channel, 
+            int dropout
+        );
+
+        torch::Tensor forward(torch::Tensor x);
+
+        ResBlockImpl resblock1{nullptr};
+        ResBlockImpl resblock2{nullptr};
+        ResBlockImpl resblock3{nullptr};
+        ResBlockImpl resblock4{nullptr};
+        torch::nn::Dropout dropout_layer{nullptr};
+        
+    };
+    TORCH_MODULE(FaceRecognitionBody);
+}
