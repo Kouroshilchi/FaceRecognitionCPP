5b45da174c35c24f3f43e63f7c6bb936b591c143
Kourosh <Kourosh.ilchi@gmail.com>
2026-06-19 20:08:50 +0330

    fix - adding more layers to model

--- Project/src/model/projector.cpp
+++ Project/src/model/projector.cpp
@@ -10,15 +10,15 @@ namespace model {
         resblock1 = register_module("resblock1", ResBlock(in_channel, 128 , 2));
         resblock2 = register_module("resblock2", ResBlock(128, 256, 1));
         resblock3 = register_module("resblock3", ResBlock(256, 512, 2));
-        resblock4 = register_module("resblock4", ResBlock(512, 512, 1));
-        resblock5 = register_module("resblock5", ResBlock(512, 512, 2));
+        resblock4 = register_module("resblock4", ResBlock(512, 1024, 1));
+        resblock5 = register_module("resblock5", ResBlock(1024, 512, 2));
         flatten = register_module("flatten", torch::nn::Flatten());
         fc1 = register_module("fc1" , torch::nn::Linear(512 * 28 * 28, 512));
-        bn1 = register_module("bn1", torch::nn::BatchNorm1d(512));
+        bn1 = register_module("bn1", torch::nn::BatchNorm1d(512)); 
         relu = register_module("relu", torch::nn::ReLU());  
-        fc2 = register_module("fc2", torch::nn::Linear(512, 256));
-        bn2 = register_module("bn2", torch::nn::BatchNorm1d(256));
-        fc3 = register_module("fc3", torch::nn::Linear(256, out_dim));
+        fc2 = register_module("fc2", torch::nn::Linear(512, 512));
+        bn2 = register_module("bn2", torch::nn::BatchNorm1d(512));
+        fc3 = register_module("fc3", torch::nn::Linear(512, out_dim));
         bn3 = register_module("bn3", torch::nn::BatchNorm1d(out_dim));
         dropout_layer = register_module("dropout_layer", torch::nn::Dropout(dropout));
     }
