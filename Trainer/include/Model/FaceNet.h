76e982f43967441040a780aa5a2b8d37b601e320
Kourosh <Kourosh.ilchi@gmail.com>
2026-06-28 16:34:50 +0330

    refactor - improve code formatting and readability in FaceNet.h

--- Trainer/include/Model/FaceNet.h
+++ Trainer/include/Model/FaceNet.h
@@ -3,6 +3,7 @@
 #include <torch/torch.h>
 #include "Model/Model.h"
 #include "Loss/ArcFace.h"
+#include "Loss/TripletLoss.h"
 #include "Loss/LossMetrics.h"
 
 namespace model {
@@ -11,17 +12,19 @@ struct FaceNetImpl : torch::nn::Module {
     FaceNetImpl(int64_t num_classes,
                int64_t embedding_dim,
                double dropout,
-               double scale = 64.0,
+               double scale  = 64.0,
                double margin = 0.5);
 
-    Loss::LossMetrics forward(const torch::Tensor& inputs, const torch::Tensor& labels);
-
+    
+    Loss::LossMetrics forward(const torch::Tensor& inputs,
+                              const torch::Tensor& labels,
+                              int64_t epoch = 1);
     torch::Tensor embed(const torch::Tensor& inputs);
-
     model::FaceRecognitionModel backbone{nullptr};
-    Loss::ArcFace arcface{nullptr};
+    // Loss::ArcFace               arcface{nullptr};
+    Loss::TripletLoss           triplet{nullptr};
 };
 
 TORCH_MODULE(FaceNet);
 
-} // namespace model
+} 
