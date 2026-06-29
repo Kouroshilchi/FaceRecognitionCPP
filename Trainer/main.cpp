26976a2b406371d3c5983a148e2d1cc770532613
Kourosh <Kourosh.ilchi@gmail.com>
2026-06-29 20:09:34 +0330

    refactor - reduce embedding dimension from 256 to 128 for model optimization

--- Trainer/main.cpp
+++ Trainer/main.cpp
@@ -110,7 +110,7 @@ int main(int argc, char* argv[]) {
         const int64_t K      = 8;    
         
 
-        const int64_t embedding_dim = 256;
+        const int64_t embedding_dim = 128;
         const double  dropout       = 0.1;
         const int64_t epochs        = 100;
         const cv::Size image_size{112, 112};
