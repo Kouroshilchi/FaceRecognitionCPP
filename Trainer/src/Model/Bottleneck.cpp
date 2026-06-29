191dbe3c227c368b4e2671ca1d31b45dc0fde405
Kourosh <Kourosh.ilchi@gmail.com>
2026-06-29 20:39:17 +0330

    refactor - add bias parameter to Conv2d layers in BottleneckImpl for consistency

--- Trainer/src/Model/Bottleneck.cpp
+++ Trainer/src/Model/Bottleneck.cpp
@@ -3,18 +3,18 @@
 namespace model {
 
 BottleneckImpl::BottleneckImpl(int64_t inplanes, int64_t planes, int64_t stride, bool downsample) {
-    conv1 = register_module("conv1", torch::nn::Conv2d(torch::nn::Conv2dOptions(inplanes, planes, 1).stride(1)));
+    conv1 = register_module("conv1", torch::nn::Conv2d(torch::nn::Conv2dOptions(inplanes, planes, 1).stride(1).bias(false)));
     bn1 = register_module("bn1", torch::nn::BatchNorm2d(planes));
 
-    conv2 = register_module("conv2", torch::nn::Conv2d(torch::nn::Conv2dOptions(planes, planes, 3).stride(stride).padding(1)));
+    conv2 = register_module("conv2", torch::nn::Conv2d(torch::nn::Conv2dOptions(planes, planes, 3).stride(stride).padding(1).bias(false)));
     bn2 = register_module("bn2", torch::nn::BatchNorm2d(planes));
 
-    conv3 = register_module("conv3", torch::nn::Conv2d(torch::nn::Conv2dOptions(planes, planes * expansion, 1).stride(1)));
+    conv3 = register_module("conv3", torch::nn::Conv2d(torch::nn::Conv2dOptions(planes, planes * expansion, 1).stride(1).bias(false)));
     bn3 = register_module("bn3", torch::nn::BatchNorm2d(planes * expansion));
 
     if (downsample || inplanes != planes * expansion) {
         downsample_layer = register_module("downsample", torch::nn::Sequential(
-            torch::nn::Conv2d(torch::nn::Conv2dOptions(inplanes, planes * expansion, 1).stride(stride)),
+            torch::nn::Conv2d(torch::nn::Conv2dOptions(inplanes, planes * expansion, 1).stride(stride).bias(false)),
             torch::nn::BatchNorm2d(planes * expansion)
         ));
     }
